5dd60cb6e7b103a0d828c629cb13f708acff8bc2
Glitch18 <atharvakn@gmail.com>
2020-10-28 23:33:57 +0530

    Fixed code

--- Archives/GeeksForGeeks/LargestNumMadeByArray.cpp
+++ Archives/GeeksForGeeks/LargestNumMadeByArray.cpp
@@ -1,51 +1,24 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include<algorithm>
-using namespace std;
+class Solution{
+public:
+	// The main function that returns the arrangement with the largest value as
+	// string.
+	// The function accepts a vector of strings
+	static bool comparator(string X,string Y){
+	    string XY = X + Y;
+	    string YX = Y + X;
+	    return XY.compare(YX) > 0 ? 1 : 0;
+	}
 
-bool comparator(string s1,string s2){
-  int len = min(s1.length(),s2.length());
-  for(int i=0;i<len;i++){
-    if(s1[i]>s2[i]) return true;
-	  else if(s2[i]>s1[i]) return false;
-  }
-
-  if(s1.length()>s2.length()){
-    int i = s2.length();
-	  while(s1[i]==s1[i-1]){
-      i++;
-      if(i==s1.length()) return true;
-    }
-    return (s1[i]>s1[i-1]);
-  }
-
-  else if(s1.length()<s2.length()){
-    int i = s1.length();
-    while(s2[i]==s2[i-1]){
-      i++;
-      if(i==s2.length()) return true;
-    }
-    cout << i << "\n";
-    return !(s2[i]>s2[i-1]);
-  }
-  else return true;
-}
-
-string printLargest(vector<string> &arr){
-  sort(arr.begin(),arr.end(),comparator);
-  string ans = "";
-	for(auto i : arr) ans += i;
-	return ans;
-}
-
-int main(){
-  //vector<string> arr = {"3","30","34","5","9"};
-  vector<string> arr = {"969","969","969","969","9","3"};
-  string ans = printLargest(arr);
-}
+	string printLargest(vector<string> &arr) {
+	    sort(arr.begin(),arr.end(),comparator);
+	    string ans = "";
+	    for(auto i : arr) ans += i;
+	    return ans;
+	}
+};
 
 /*
+My code that didn't work for some reason
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -66,15 +39,15 @@ public:
 	            i++;
 	            if(i==s1.length()) return true;
 	        }
-	        return (s1[i]>s1[i-1]) ? true : false;
+	        return (s1[i]>s1[i-1]);
 	    }
 	    else if(s1.length()<s2.length()){
 	        int i = s1.length();
 	        while(s2[i]==s2[i-1]){
 	            i++;
-	            if(i==s1.length()) return true;
+	            if(i==s2.length()) return true;
 	        }
-	        return (s2[i]>s2[i-1]) ? true : false;
+	        return !(s2[i]>s2[i-1]);
 	    }
 	    else return true;
 	}
@@ -82,7 +55,10 @@ public:
 	string printLargest(vector<string> &arr) {
 	    sort(arr.begin(),arr.end(),comparator);
 	    string ans = "";
-	    for(auto i : arr) ans += i;
+	    for(auto i : arr){
+				ans += i;
+				ans += " ";
+			}
 	    return ans;
 	}
 };
